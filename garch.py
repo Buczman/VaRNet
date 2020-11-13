706688a8ac22ec9a8b32eaf41441fa6844eff104
Buczman <mateusz.buczynski@interia.pl>
2020-11-13 01:44:46 +0100

    repository cleaning; added garch and caviar ready to run

--- garch.py
+++ garch.py
@@ -1,82 +1,39 @@
-import tensorflow as tf
 import pandas as pd
-import matplotlib.pyplot as plt
 import numpy as np
-import scipy.stats
 import torch
+import skewstudent
+from loss import hansen_garch_skewed_student_loss, caviar_loss
+from nets import GARCHSkewedTStudent
+from utils import predict_rolling
+
+
 
 data = pd.read_csv('./data/wig.csv').set_index('Data')
-data['log_returns'] = data['Zamkniecie'].rolling(2).apply(lambda x: np.log(x[1]/x[0]), raw=True)
-
+data['log_returns'] = data['Zamkniecie'].rolling(2).apply(lambda x: np.log(x[1] / x[0]), raw=True)
 
 dataset = data.loc[(data.index > '2005-01-01')]
 dataset2 = dataset.iloc[1:501]
 
 history_size = 30
 
-def univariate_data(dataset, start_index, end_index, history_size, target_size):
-
-    start_index = start_index + history_size
-    if end_index is None:
-        end_index = len(dataset) - target_size
-
-    for i in range(start_index, end_index):
-        variance = np.zeros((history_size, 1))
-        indices = range(i-history_size, i)
-        X_train = np.reshape(dataset[indices], (history_size, 1))
-        # Reshape data from (history_size,) to (history_size, 1)
-        yield X_train, np.array(dataset[i+target_size])
 
 
-def garch_loss(true, vol):
-    return 1 / 2 * (torch.log(vol) + true ** 2 / vol)  #    + tf.math.log(2 * tf.constant(np.pi))
+model = GARCHSkewedTStudent()
+loss_function = hansen_garch_skewed_student_loss
+optimizer = torch.optim.Adam([{'params': model.parameters()},
+                              {'params': [model.skewness, model.df], 'lr': 1e-2}], lr=3e-4)
 
 
-class LSTM(torch.nn.Module):
-    def __init__(self, input_size=1, hidden_layer_size=100, output_size=1):
-        super().__init__()
-        self.hidden_layer_size = hidden_layer_size
+epochs = 45
 
-        self.lstm = torch.nn.LSTM(input_size, hidden_layer_size)
-
-        self.linear = torch.nn.Linear(hidden_layer_size, output_size)
-
-        self.hidden_cell = (torch.zeros(1,1,self.hidden_layer_size),
-                            torch.zeros(1,1,self.hidden_layer_size))
-        self.sigmoid = torch.nn.Sigmoid()
-
-    def forward(self, input_seq):
-        lstm_out, self.hidden_cell = self.lstm(input_seq.view(len(input_seq) ,1, -1), self.hidden_cell)
-        predictions = self.sigmoid(self.linear(lstm_out.view(len(input_seq), -1)))
-        return predictions[-1]
-
-model = LSTM()
-loss_function = garch_loss
-optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
-
-epochs = 15
-
-for i in range(epochs):
-    for seq, labels in univariate_data(dataset2.log_returns.to_numpy(), 0, None, 30, 0):
-        optimizer.zero_grad()
-        model.hidden_cell = (torch.zeros(1, 1, model.hidden_layer_size),
-                        torch.zeros(1, 1, model.hidden_layer_size))
-
-        X_train = torch.tensor(np.expand_dims(seq, axis=1)).float()
-        y_pred = model(X_train)
-        single_loss = loss_function(torch.tensor(labels).float(), y_pred)
-        single_loss.backward()
-        optimizer.step()
-
-    print(f'epoch: {i:3} loss: {single_loss.tolist()[0]:10.8f}')
-
-    if i == epochs-1:
-        print(torch.sqrt(y_pred) * scipy.stats.norm.ppf(0.025))
+hacky_list = []
 
 
 
 
-
-
-# dataset['var'] = dataset.log_returns.rolling(251).apply(predict_rolling, raw=True)
-# dataset.to_csv('./data_garch.csv')
+dataset2[['var']] = dataset2.log_returns.rolling(251).apply(predict_rolling, kwargs={'model': model,
+                                                                                     'loss_function': loss_function,
+                                                                                     'optimizer': optimizer,
+                                                                                     'epochs': epochs})
+pd.DataFrame(hacky_list).to_csv('./data_garch_sst_params.csv')
+dataset2.to_csv('./data_garch_sst.csv')
--- test.py
+++
@@ -1,9 +0,0 @@
-from rl import ValueAtRiskEnvironment
-import random
-
-env = ValueAtRiskEnvironment()
-obs = env.reset()
-for i in range(2000):
-  action = random.uniform(-1, 1)
-  obs, rewards, done, info = env.step(action)
-  env.render()
